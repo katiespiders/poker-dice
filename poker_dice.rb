fce3b004c145f9615f07fc18795db000c5bc4cda
Katie <kab761@gmail.com>
2014-11-03 16:49:59 -0800

    all hand scoring works

--- poker_dice.rb
+++ poker_dice.rb
@@ -1,7 +1,6 @@
 module PokerDice
   class Die
     FACES = %w[ 9 T J Q K A ]
-    # FACES.collect! { |k, v| }
 
     attr_reader :top_face
 
@@ -14,40 +13,58 @@ module PokerDice
     end
   end
 
+  class LoadedDie
+    attr_reader :top_face
+
+    def initialize(top_face)
+      @top_face = top_face
+    end
+  end
+
   class Hand
     attr_accessor :dice
 
-    def initialize
-      @dice = []
-      5.times { @dice << Die.new.top_face }
+    def initialize(dice = [])
+      @dice = dice
+      if @dice == []
+        5.times { @dice << Die.new.top_face }
+      end
 
       straight?
-      matches?
+      counts_hash
 
     end
 
     def straight?
-
+      sorted_dice = @dice.sort
+      (sorted_dice == %w[9 J K Q T]) || (sorted_dice == %w[ A J K Q T])
     end
 
-    def matches?
+    def counts_hash
       count = 0
       card_hash = {}
       for card in @dice
         count = @dice.count {|x| x == card }
-        # if count > 1
-          card_hash[card] = count
-        # end
+        card_hash[card] = count
       end
-      puts card_hash
       card_hash
     end
 
-    def hands(hash)
-      for item in hash
-
+    def scores(hash)
+      @score = case
+      when hash.values.include?(5) ; "five of a kind"
+      when hash.values.include?(4) ; "four of a kind"
+      when hash.values.include?(3)
+        if hash.values.include?(2)
+          "full house"
+        else
+          "three of a kind"
+        end
+      when hash.values.count(2) == 2 ; "two pairs"
+      when hash.values.count(2) == 1 ; "one pair"
+      else
+        "bust"
       end
     end
-
   end
 end
--- spec/poker_dice_spec.rb
+++ spec/poker_dice_spec.rb
@@ -17,23 +17,81 @@ describe PokerDice::Die do
 end
 
 describe PokerDice::Hand do
-  let(:hand) {PokerDice::Hand.new}
+  describe "with random dice"
+    let(:hand) {PokerDice::Hand.new}
 
-  specify "creates a hand with length 5" do
-    expect(hand.dice.class).to eq Array
-    expect(hand.dice.length).to eq 5
+    specify "creates a hand with length 5" do
+      expect(hand.dice.class).to eq Array
+      expect(hand.dice.length).to eq 5
+    end
+
+    specify "adds string objects to hand" do
+      expect(hand.dice[3].class).to eq String
+    end
+
+    specify "adds string representing a valid face" do
+      expect(PokerDice::Die::FACES).to include(hand.dice[3])
+    end
+
+    specify "creates a hash of cards and their count" do
+      expect(hand.counts_hash.class).to eq Hash
+    end
   end
 
-  specify "adds string objects to hand" do
-    expect(hand.dice[3].class).to eq String
-  end
+describe PokerDice::Hand do
+  describe "with loaded dice" do
 
-  specify "adds string representing a valid face" do
-    expect(PokerDice::Die::FACES).to include(hand.dice[3])
-  end
+    def hand_with(face_values)
+      dice = face_values.map { |value| PokerDice::LoadedDie.new(value).top_face }
+      PokerDice::Hand.new( dice )
+    end
 
-  specify "creates a hash of cards and their count" do
-    expect(hand.matches?.class).to eq Hash
-  end
+    it "counts_hash returns a hash" do
+      hand = hand_with(%w[ Q Q Q Q Q ])
+      expect(hand.counts_hash.class).to eq Hash
+    end
 
+    it "straight? returns true for the 2 possible values" do
+      hand = hand_with(%w[ 9 T J Q K])
+      expect(hand.straight?).to eq true
+      hand = hand_with(%w[ T J Q K A])
+      expect(hand.straight?).to eq true
+    end
+
+    it "scores returns five of a kind" do
+      hand = hand_with(%w[ J J J J J])
+      expect(hand.scores(hand.counts_hash)).to eq "five of a kind"
+    end
+
+    it "scores returns four of a kind" do
+      hand = hand_with(%w[ J J J J T])
+      expect(hand.scores(hand.counts_hash)).to eq "four of a kind"
+    end
+
+    it "scores returns full house" do
+      hand = hand_with(%w[ J J J Q Q])
+      expect(hand.scores(hand.counts_hash)).to eq "full house"
+    end
+
+    it "scores returns three of a kind" do
+      hand = hand_with(%w[ J J J Q K])
+      expect(hand.scores(hand.counts_hash)).to eq "three of a kind"
+    end
+
+    it "scores returns two pairs" do
+      hand = hand_with(%w[J J Q Q K])
+      expect(hand.scores(hand.counts_hash)).to eq "two pairs"
+    end
+
+    it "scores returns one pair" do
+      hand = hand_with(%w[J J Q K A])
+      expect(hand.scores(hand.counts_hash)).to eq "one pair"
+    end
+
+    it "scores returns bust" do
+      hand = hand_with(%w[9 T J K A])
+      expect(hand.scores(hand.counts_hash)).to eq "bust"
+    end
+
+  end
 end
